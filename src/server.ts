0ccfd9aae3749cf51296a8373000dfb8629de9c4
Jason Rowland <jason@snowcapstudios.com>
2018-08-08 07:01:10 -0600

    Work with helm/k8s

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,27 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
+import * as WebSocket from 'ws';
+import * as http from 'http';
+import * as path from 'path';
+
 
 const app: express.Application = express();
 
-app.use((req: Request, res: Response) => {
-  res.send('hello world');
+const dir = path.resolve('public');
+console.log(dir);
+app.use(express.static(dir));
+
+const server: http.Server = app.listen(3000, () => {
+  console.log('Listening on port 3000');
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+const wss = new WebSocket.Server({ server: server });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+ 
+  console.log('connected');
+  ws.send('something from server');
 });
